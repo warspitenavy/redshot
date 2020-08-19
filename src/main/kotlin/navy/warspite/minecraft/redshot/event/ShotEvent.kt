1c9b58216fdc61ddf435e7d67f503d7f99464d14
warspite1126 <mail@warspite.navy>
2020-08-20 04:14:07 +0900

    バーストの連打対策が！！難しい！！！

--- src/main/kotlin/navy/warspite/minecraft/redshot/event/ShotEvent.kt
+++ src/main/kotlin/navy/warspite/minecraft/redshot/event/ShotEvent.kt
@@ -3,40 +3,42 @@ package navy.warspite.minecraft.redshot.event
 import navy.warspite.minecraft.redshot.LoadWeapons
 import navy.warspite.minecraft.redshot.Main
 import navy.warspite.minecraft.redshot.util.GetColoured.colouredMessage
-import org.bukkit.Bukkit
-import org.bukkit.NamespacedKey
-import org.bukkit.Sound
-import org.bukkit.entity.Player
+import org.bukkit.*
 import org.bukkit.entity.Snowball
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
-import org.bukkit.event.entity.EntityDamageByEntityEvent
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.persistence.PersistentDataType
 import org.bukkit.scheduler.BukkitRunnable
 
 object ShotEvent : Listener {
     private val plugin = Main.instance
-    private val clickPlayers = linkedMapOf<String, Long>()
+    private val shootingPlayers = linkedMapOf<String, Boolean>()
+    private val burstPlayers = linkedMapOf<String, Boolean>()
     private val shootCount = linkedMapOf<String, Int>()
 
     @EventHandler
     fun catchShotEvent(e: PlayerInteractEvent) {
         if (e.action == Action.RIGHT_CLICK_AIR ||
-            e.action == Action.RIGHT_CLICK_BLOCK) shot(e)
+            e.action == Action.RIGHT_CLICK_BLOCK
+        ) shot(e)
     }
 
     private fun shot(e: PlayerInteractEvent) {
         if (shootCount[e.player.name] == null) shootCount[e.player.name] = 0
+//        e.player.sendMessage(colouredMessage(shootingPlayers[e.player.name].toString()))
+        if (shootingPlayers[e.player.name] == null) shootingPlayers[e.player.name] = false
+        if (burstPlayers[e.player.name] == null) shootingPlayers[e.player.name] = false
+
         val itemStack = e.player.inventory.itemInMainHand
         val key = NamespacedKey(plugin, "RedShotKey")
         val itemMeta = itemStack.itemMeta
         val container = itemMeta?.persistentDataContainer
         val id = (if (container != null) {
-            if (container.has(key, PersistentDataType.STRING)) {
-                container.get(key, PersistentDataType.STRING)
-            } else null
+            if (container.has(key, PersistentDataType.STRING)
+            ) container.get(key, PersistentDataType.STRING)
+            else null
         } else null)
             ?: return
 
@@ -48,28 +50,22 @@ object ShotEvent : Listener {
         }
 
         val delayBetweenShots = shooting["delayBetweenShots"] as Int?
-        val projectileAmount = shooting["projectileAmount"] as Int
-        val projectileDamage = shooting["projectileDamage"] as Int
-        val projectileSpeed = shooting["projectileSpeed"] as Int
-        val projectileType = shooting["projectileType"] as String
-        val shootSounds = shooting["shootSounds"]
+        val projectileAmount = (shooting["projectileAmount"] ?: 1) as Int
+        val projectileDamage = (shooting["projectileDamage"] ?: 0) as Int
+        val projectileSpeed = (shooting["projectileSpeed"] ?: 40) as Int
+        val projectileType = ((shooting["projectileType"] ?: "snowball") as String)
+        val shootSounds = shooting["shootSounds"] as ArrayList<*>?
 
         val fullyAutomatic = weapon["fullyAutomatic"] as LinkedHashMap<*, *>?
         val fireRate = fullyAutomatic?.get("fireRate") as Int
 
-        fun getRate(i: Int): Int {
-            val min = 240 + (i * 60)
-            return min / 60
-        }
-
         fun playShootSounds() {
-            shootSounds as ArrayList<*>?
             shootSounds?.forEach {
                 it as String
                 val soundParam = it.split('-')
                 Bukkit.getScheduler().runTaskLater(
                     plugin, Runnable {
-                        e.player.playSound(
+                        e.player.location.world?.playSound(
                             e.player.location,
                             Sound.valueOf(soundParam[0]),
                             soundParam[1].toFloat(),
@@ -94,74 +90,101 @@ object ShotEvent : Listener {
         }
 
         fun repeatShoot() {
+//            fun getRate(i: Int): Int {
+//                val min = 240 + (i * 60)
+//                return min / 60
+//            }
+
+            if (shootingPlayers[e.player.name]!!) {
+                e.player.sendMessage(colouredMessage("shot"))
+                return
+            }
+
             var count = 0
-            val period: ArrayList<Long> = when(fireRate) {
-                1 -> arrayListOf(1,1,1,1)
-                2 -> arrayListOf(1,2,1,2)
-                3 -> arrayListOf(1,2,2,2)
-                4 -> arrayListOf(2,2,2,2)
-                5 -> arrayListOf(2,2,2,3)
-                6 -> arrayListOf(2,3,2,3)
-                7 -> arrayListOf(2,3,3,3)
-                8 -> arrayListOf(3,3,3,3)
-                9 -> arrayListOf(3,3,3,4)
-                10 -> arrayListOf(3,4,3,4)
-                11 -> arrayListOf(3,4,4,4)
-                12 -> arrayListOf(4,4,4,4)
-                13 -> arrayListOf(4,4,4,5)
-                14 -> arrayListOf(4,5,4,5)
-                15 -> arrayListOf(4,5,5,5)
-                16 -> arrayListOf(5,5,5,5)
-                else -> arrayListOf(1,1,1,1)
+            val perBurst = when (fireRate) {
+                1 -> arrayListOf(1, 1, 1, 1)
+                2 -> arrayListOf(1, 2, 1, 2)
+                3 -> arrayListOf(1, 2, 2, 2)
+                4 -> arrayListOf(2, 2, 2, 2)
+                5 -> arrayListOf(2, 2, 2, 3)
+                6 -> arrayListOf(2, 3, 2, 3)
+                7 -> arrayListOf(2, 3, 3, 3)
+                8 -> arrayListOf(3, 3, 3, 3)
+                9 -> arrayListOf(3, 3, 3, 4)
+                10 -> arrayListOf(3, 4, 3, 4)
+                11 -> arrayListOf(3, 4, 4, 4)
+                12 -> arrayListOf(4, 4, 4, 4)
+                13 -> arrayListOf(4, 4, 4, 5)
+                14 -> arrayListOf(4, 5, 4, 5)
+                15 -> arrayListOf(4, 5, 5, 5)
+                16 -> arrayListOf(5, 5, 5, 5)
+                else -> arrayListOf(1, 1, 1, 1)
             }
-            val pattern = when(fireRate) {
-                1 -> arrayListOf(0,0,0,0)
-                2 -> arrayListOf(0,0,0,0)
-                3 -> arrayListOf(1,1,1,1)
-                4 -> arrayListOf(1,1,1,1)
-                5 -> arrayListOf(1,1,1,0)
-                6 -> arrayListOf(0,1,0,1)
-                7 -> arrayListOf()
-                8 -> arrayListOf(3,3,3,3)
-                9 -> arrayListOf(3,3,3,4)
-                10 -> arrayListOf(3,4,3,4)
-                11 -> arrayListOf(3,4,4,4)
-                12 -> arrayListOf(4,4,4,4)
-                13 -> arrayListOf(4,4,4,5)
-                14 -> arrayListOf(4,5,4,5)
-                15 -> arrayListOf(4,5,5,5)
-                16 -> arrayListOf(5,5,5,5)
-                else -> arrayListOf(1,1,1,1)
+
+            val playerShootCount = shootCount[e.player.name]!!
+
+            val period = when {
+                fireRate <= 6 -> arrayListOf(2, 2, 2, 2)
+                fireRate == 7 -> arrayListOf(2, 2, 2, 1)
+                fireRate >= 8 -> arrayListOf(1, 1, 1, 1)
+                else -> arrayListOf(1, 1, 1, 1)
             }
-            shootCount[e.player.name] = shootCount[e.player.name]!!.plus(1)
+
+            println("purBurst:${perBurst[playerShootCount]}")
+            println("period:${period[playerShootCount]}")
+            println("count:$playerShootCount")
 
             object : BukkitRunnable() {
                 override fun run() {
-                    if (count < period[shootCount[e.player.name]!!]) {
-                        println("count:${shootCount[e.player.name]}")
-                        println(count)
+                    if (count < perBurst[playerShootCount]) {
+                        shootingPlayers[e.player.name] = true
                         playShootSounds()
                         shootProjectile()
+                        e.player.sendMessage(colouredMessage(shootingPlayers[e.player.name].toString()))
                         count++
-                    } else cancel()
+                    } else {
+                        shootingPlayers[e.player.name] = false
+                        e.player.sendMessage(colouredMessage(shootingPlayers[e.player.name].toString()))
+                        cancel()
+                    }
                 }
-            }.runTaskTimer(plugin, 0, 2)
+            }.runTaskTimer(plugin, 0, period[playerShootCount].toLong())
+            shootCount[e.player.name] = shootCount[e.player.name]!!.plus(1)
             if (shootCount[e.player.name]!! > 3) shootCount[e.player.name] = 0
         }
-        if (fullyAutomatic != null) repeatShoot()
-    }
-}
+
+        if (delayBetweenShots == null) {
+            repeatShoot()
+//            if (shootingPlayers[e.player.name]!!) {
+//                e.player.sendMessage(colouredMessage("You are shot"))
+//                return
+//            }
+//            if (shootingPlayers[e.player.name] == false) repeatShoot()
+//            if (shootingPlayers[e.player.name] == true) {
+//                if (burstPlayers[e.player.name] == true) return
+//                burstPlayers[e.player.name] = true
+//                Bukkit.getScheduler().runTaskLater(
+//                    plugin, Runnable {
+//                        repeatShoot()
+//                        e.player.sendMessage(colouredMessage("delay"))
+//                        if (burstPlayers[e.player.name] == true) burstPlayers[e.player.name] = false
+//                    }, 4
+//                )
+//            }
+        }
 
 //        val currTime = System.currentTimeMillis()
 //        if (!clickPlayers.containsKey(e.player.name)) {
 //            if (clickPlayers.isEmpty()) {
 //                clickPlayers[e.player.name] = currTime
-////                return
+//                return
 //            }
 //        }
 //        val lastClick = clickPlayers[e.player.name]!!
-//        if (currTime - lastClick > 250) {
+//        if (currTime - lastClick > (delayBetweenShots / 20) * 1000) {
 //            clickPlayers.remove(e.player.name)
 //        } else {
 //            clickPlayers[e.player.name] = currTime
 //        }
+    }
+}
